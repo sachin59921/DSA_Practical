16e40aa546934a74a1cf70d6efd6245de54048bf
SACHIN KUMAR PAL <101247442+sachin59921@users.noreply.github.com>
2023-10-12 09:38:41 -0700

    Delete TreeTraversal directory

--- TreeTraversal/TreeTraversal.h
+++
@@ -1,9 +0,0 @@
-#ifndef TREE_TRAVERSAL
-#define TREE_TRAVERSAL
-#include "../BinaryTrees/BinaryTrees.h"
- 
-void inorderTraversal(TreeNode* root);
-void preorderTraversal(TreeNode* node);
-void postorderTraversal(TreeNode* node); 
-
-#endif
