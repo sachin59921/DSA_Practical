26caaf0d65fe843504d20a2a399c48b90e6aea71
SACHIN KUMAR PAL <101247442+sachin59921@users.noreply.github.com>
2023-10-12 09:38:06 -0700

    Delete DSA_Practical directory

--- DSA_Practical/EvaluatePostfix/EvaluatePostfix.h
+++
@@ -1,7 +0,0 @@
-#ifndef EVALUATE_POSTFIX_H
-#define EVALUATE_POSTFIX_H
-
-// Function prototypes for evaluating postfix expressions
-void evaluatePostfix();
-
-#endif
--- DSA_Practical/InfixToPostfix/InfixToPostfix.h
+++
@@ -1,7 +0,0 @@
-#ifndef INFIX_TO_POSTFIX_H
-#define INFIX_TO_POSTFIX_H
-
-// Function prototypes for infix-to-postfix conversion
-void infixToPostfix();
-
-#endif
